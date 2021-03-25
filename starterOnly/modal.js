a92bee3d4b78fe9828563b7eea8cdcf669baadf7
Tomss <tsemeri@gmail.com>
2021-03-25 15:45:33 +0100

    Add email verification
    Quand l'email tapé est invalide, un message d'erreur s'affiche sous le input, qui arbore une bordure rouge

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,3 +30,53 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+//******************************
+
+//Vise la div qui a l'ID 'loginForm' de mon html
+let form = document.querySelector('#loginForm');
+//Ecouter la modification de l'email
+form.email.addEventListener('change', function() {
+  validEmail(this);
+})
+
+console.log(form.email);
+
+const validEmail = function (inputEmail) {
+
+
+  let checkMail = document.getElementById('email');
+  let emailRegExp = new RegExp (
+    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$','g');
+  let testEmail = emailRegExp.test(inputEmail.value);
+  let small = document.getElementById('mailError');
+
+if (testEmail) {
+  small.innerHTML ='';
+  return true;
+} else {
+  small.innerHTML = 'Email invalide';
+  small.style.color = 'red';
+  checkMail.border.style = '1px solid red';
+  return false;
+};
+
+
+
+
+}
+
+/* Email valid or not
+
+function validate() {
+    let checkMail;
+
+    checkMail = document.getElementById("email");
+
+    if (checkMail(email)) {
+      alert('Adresse e-mail valide');
+    }
+    else {
+      alert('Adresse e-mail non valide');
+    }
+    return false;
+} */
